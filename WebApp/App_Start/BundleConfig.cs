d3b023d5d5ee82614746c122bccddaaabc68d35f
Craig Wilson <craig@wilsoncg.net>
2014-02-23 22:23:27 +0000

    Move upload script

--- WebApp/App_Start/BundleConfig.cs
+++ WebApp/App_Start/BundleConfig.cs
@@ -14,12 +14,7 @@ namespace WebApp
                 "~/Scripts/jquery.validate*",
                 "~/Scripts/jquery.fileupload.js",
                 "~/Scripts/jquery.fileupload-process.js",
-                "~/Scripts/jquery.fileupload-validate.js",
-                "~/Scripts/upload.js"));
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include("~/Scripts/jquery-ui-{version}.js"));
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryfileupload").Include("~/Scripts/jquery.fileupload.js"));
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryupload").Include("~/Scripts/upload.js"));
+                "~/Scripts/jquery.fileupload-validate.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
